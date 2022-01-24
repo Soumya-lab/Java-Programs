7ddf64d4c470b2f5baa3c7d4f38562e150ff4b8a
Soumya <55018093+Soumya-lab@users.noreply.github.com>
2022-01-24 10:27:52 +0530

    Update Pattern1.java
    Rectangle Pattern
    *****
    *****
    *****
    *****
    *****

--- Pattern/Pattern1.java
+++ Pattern/Pattern1.java
@@ -1,18 +1,19 @@
-public class Left_Triangle {
+import java.util.Scanner;
+public class Pattern1 
+{
     public static void main(String args[])
     {
-        int i,j,k;
-        for(i=1;i<=5;i++)
+        int i,j,n;
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        n= in.nextInt();
+        for(i=1;i<=n;i++)
         {
-            for(j=4;j>=i;j--)
-            {
-                System.out.print(" ");
-            }
-            for(k=1;k<=i;k++)
+            for(j=1;j<=n;j++)
             {
                 System.out.print("*");
             }
-            System.out.println();
+            Sytem.out.println();
         }
     }
     
